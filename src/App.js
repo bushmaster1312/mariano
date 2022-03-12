fd5e23a8717b1dc06f07e322071cdbf18e6796fa
bushmaster1312 <marianodominguez1992@gmail.com>
2022-03-12 16:12:07 -0300

    segundo intento

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Carousel from './components/carousel-1/Carousel';
+import cards from './components/cards';
+import Cards from './components/cards';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <Navbar/>
+     <div><Carousel/></div>
+     <div className='cards'><Cards/></div>
+    </>
+    
+
+   
+   
+
+  )
+    
+ 
+};
 
 export default App;
---
+++ src/components/carousel-1/Carousel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import carrousel from "../../assets/img/carrousel-3.jpg"
+import "../carousel-1/carousel.css"
+
+const Carousel = () => {
+  return (
+    <div className="carousel">
+      <img src={carrousel} alt="homerosapienss"/> 
+    </div>
+    
+  )
+}
+
+export default Carousel
